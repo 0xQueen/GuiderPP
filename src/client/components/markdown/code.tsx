704df33a5bb8f15debe7ec1074189d09a9e021aa
0xQueen <anastasiapevac@gmail.com>
2024-05-15 08:37:03 +0300

    a

---
+++ src/client/components/markdown/code.tsx
@@ -0,0 +1,65 @@
+import { useCallback, useRef, useState } from 'react';
+import { Icon } from '../icon';
+import type { MarkdownProps } from './types';
+
+function useCopy() {
+  const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+  const [copied, setCopied] = useState(false);
+  const copy = useCallback((contents: string) => {
+    navigator.clipboard
+      .writeText(contents)
+      .then(() => {
+        setCopied(true);
+        if (timeoutRef.current !== null) clearTimeout(timeoutRef.current);
+        timeoutRef.current = setTimeout(() => {
+          setCopied(false);
+        }, 2000);
+      })
+      .catch((err: any) => {
+        // eslint-disable-next-line no-console -- need to do something with the error :P
+        console.error('Failed to copy to clipboard', err);
+      });
+  }, []);
+
+  return { copied, copy };
+}
+
+export function CopyButton(props: { copied?: boolean; onClick: () => void }) {
+  return (
+    <button
+      type="button"
+      onClick={props.onClick}
+      className="gd-rounded neato-guider-copy gd-text-text gd-text-opacity-50 hover:gd-bg-text hover:gd-text-white hover:gd-text-opacity-100 hover:gd-bg-opacity-25 gd-text-lg gd-size-8 gd-flex gd-items-center gd-justify-center active:gd-scale-110 gd-transition-transform gd-duration-150 gd-py-2 gd-px-4"
+    >
+      {props.copied ? (
+        <Icon
+          icon="ph:check-bold"
+          className="gd-text-semanticTipLighter gd-mr-2"
+        />
+      ) : (
+        <Icon icon="mingcute:copy-2-fill" className="gd-mr-2" />
+      )}
+    </button>
+  );
+}
+
+export function MarkdownCodeBlock(props: MarkdownProps) {
+  const ref = useRef<HTMLDivElement>(null);
+  const { copy, copied } = useCopy();
+  const copyCode = useCallback(() => {
+    const el = ref.current;
+    if (!el) return;
+    const code = el.querySelector('code');
+    if (!code) return;
+    copy(code.innerText);
+  }, [copy]);
+
+  if (props.attrs['data-theme']) return props.children;
+
+  return (
+    <figure className="neato-guider-codeblock" ref={ref} {...props.attrs}>
+      <CopyButton copied={copied} onClick={copyCode} />
+      {props.children}
+    </figure>
+  );
+}
---
+++ src/client/partials/header/sidebar-mobile-nav.tsx
@@ -0,0 +1,88 @@
+import classNames from 'classnames';
+import { Fragment, useEffect, useState } from 'react';
+import { makeKey } from 'src/client/utils/make-key';
+import { usePageSwitch } from 'src/client/hooks/use-page-switch';
+import { GuiderSidebarContent } from '../sidebar';
+import { Icon } from '../../components/icon';
+import type { TabsChildren } from '../../../theme/components/site';
+import type { CustomComponentComponent } from '../../../theme/components/component';
+import { SidebarStarLink } from '../sidebar/star-link';
+import { SidebarSeparator } from '../sidebar/separator';
+
+function CustomComponentTab(props: { component: CustomComponentComponent }) {
+  return <Fragment>{props.component.component?.()}</Fragment>;
+}
+
+export function SidebarMobileNav(props: { tabs: TabsChildren[] }) {
+  const [navOpen, setNavOpen] = useState(false);
+  usePageSwitch(() => {
+    setNavOpen(false);
+  }, []);
+
+  const toggleButton = (
+    <button
+      type="button"
+      onClick={() => {
+        setNavOpen(!navOpen);
+      }}
+      className="!gd-bg-bgDark gd-gap-2 gd-py-1.5 gd-px-2 gd-text-left md:gd-w-48 hover:!gd-bg-bgLight active:gd-scale-95 hover:gd-text-textHeading gd-transition-[color,transform] gd-border gd-border-bgLightest gd-rounded-md gd-flex gd-items-center"
+    >
+      <Icon icon="mingcute:menu-fill" className="gd-opacity-50 gd-text-text" />
+    </button>
+  );
+
+  useEffect(() => {
+    document.body.setAttribute(
+      'data-mobile-stop-overflow',
+      navOpen ? 'true' : 'false',
+    );
+    return () => {
+      document.body.removeAttribute('data-mobile-stop-overflow');
+    };
+  }, [navOpen]);
+
+  return (
+    <div className="gd-py-1">
+      {toggleButton}
+      <div
+        className={classNames(
+          'gd-fixed gd-top-0 gd-left-0 gd-w-full gd-h-full gd-bg-black gd-bg-opacity-50',
+          navOpen ? 'gd-opacity-100' : 'gd-opacity-0 gd-pointer-events-none',
+        )}
+        onClick={() => {
+          setNavOpen(false);
+        }}
+      />
+      <aside
+        className={classNames(
+          'gd-fixed gd-w-[350px] gd-max-w-[75vw] gd-top-0 gd-left-0 gd-h-screen !gd-h-[100dvh] gd-bg-bg gd-z-[100] gd-px-6 gd-py-10 gd-transition-transform gd-duration-150 gd-border-r gd-border-line gd-overflow-y-auto gd-space-y-4',
+          navOpen ? 'gd-translate-x-0' : '-gd-translate-x-full',
+        )}
+      >
+        <div className="-gd-mx-4">
+          {props.tabs.map((v, i) => {
+            const key = makeKey(i, v);
+            if (v.type === 'component')
+              return <CustomComponentTab key={key} component={v} />;
+            if (v.type === 'link')
+              return (
+                <SidebarStarLink
+                  key={key}
+                  link={{
+                    ...v,
+                    icon: v.icon ?? 'ph:book-open-fill',
+                    exact: v.exact ?? false,
+                  }}
+                />
+              );
+            return null;
+          })}
+          {props.tabs.length > 0 ? <SidebarSeparator /> : null}
+        </div>
+        <div>
+          <GuiderSidebarContent />
+        </div>
+      </aside>
+    </div>
+  );
+}
